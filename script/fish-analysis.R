abf07a8b0b38b1dd798b1b9eb9d3e6ee7d3f7b21
JadaRasmussen <jadar13@uw.edu>
2021-11-16 10:21:30 -0800

    edited the code to fit the new file name

--- script/fish-analysis.R
+++ script/fish-analysis.R
@@ -1,6 +1,6 @@
 
 
-fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv") 
+fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv") 
 
 library(dplyr)
 
@@ -16,3 +16,4 @@ fish_data_cat <- fish_data %>%
 fish_data_cat <- fish_data %>%
   filter(scalelength > 1) %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
