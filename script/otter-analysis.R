978886c1d4cc0f50c7b79dbb0a1da1e5d366ccc3
JadaRasmussen <jadar13@uw.edu>
2021-11-26 14:26:34 -0800

    load in the data and renamed it Otter_data. loaded in the correct programs and filtered out the data explaining what I will be looking at closer

--- script/otter-analysis.R
+++ script/otter-analysis.R
@@ -3,3 +3,15 @@
 otter_data <- read.csv("data/Bowden-ParryOtterdata.csv")
 
 library(dplyr)
+library(ggplot2)
+
+#gather new information (answers to questions) regarding your dataset.
+#What food do the otters prefer more; trout, mussels, or crab?
+#using the columns of data, foodtype, totalbeg and begreceived.
+#I need begreceived to be > 0 to exclude trials that did not receive food when they wanted it. 
+# I also made the totalbeg > 0 so I could see if the otters wanted that food or not. 
+
+otter_data %>%
+  filter(begreceived > 0, totalbeg > 0) %>% 
+  select(foodtype, totalbeg)
+
